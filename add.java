910b862c08cb217468c80ad26ae25183ed6ca78e
ajay1293-git <ajaypathipati29@gmail.com>
2025-12-16 21:46:22 +0530

    add.java

--- add.java
+++ add.java
@@ -1,5 +1,6 @@
 public class add { 
 	public static void main(string[] args){
 		int a=10, int b=20;
-		system.out.ptintin("The sum is:"+(public class add {
+		system.out.ptintin("The sum is:"+(a+b));
 		}
+}
